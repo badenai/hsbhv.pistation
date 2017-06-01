d6ce26b834fcba311dfcd96e6950d5393369cf22
ryildiz94 <receb@gmx.de>
2017-06-01 14:30:19 +0200

    Client modifiziert, Library MFRC522 hinzugefuegt

--- sensor_start.py
+++ sensor_start.py
@@ -4,10 +4,17 @@ import json
 from autobahn.asyncio.websocket import WebSocketClientProtocol, \
     WebSocketClientFactory
 from itertools import cycle
+import MFRC522
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 STARTSTATION = 'STARTSTATION'
 
 class StartStation(object):
+
     class StationStartProtocol(WebSocketClientProtocol):
         def onOpen(self):
             auth = {'auth': STARTSTATION}
@@ -15,6 +22,11 @@ class StartStation(object):
 
         def onMessage(self, payload, isBinary):
             if not isBinary:
+
+                if payload == 'kannbeginnen':
+                    serverFlag = True
+                if payload == 'reset':
+
                 res = json.loads(payload.decode('utf8'))
                 print("Result received: {}".format(res))
                 #self.sendClose()
@@ -25,9 +37,15 @@ class StartStation(object):
             #loop.stop()
 
     def __init__(self, host, port):
+        self.serverFlag = False
+        self.rfidFlag = False
         self.host = host
         self.port = port
         self.protocol = None
+
+    def resetAll(self):
+        self.rfidFlag = False
+        self.serverFlag = False
 
     def sendMessage(self, payload):
         if self.protocol:
@@ -36,25 +54,51 @@ class StartStation(object):
             print('Can not send Message.')
 
     async def callbackRFID(self, rfid):
+        print(rfid)
+
+        rfidMessage = { "type": "RFID",
+                        "value": rfid}
+
+        self.sendMessage(json.dumps(rfidMessage).encode('utf8'))
         # RFID empfangen
-        pass
+        #pass
 
     async def callbackGPIO(self, data):
         # GPIO Messung
         print(data)
 
+        gpioMessage = {
+            "type": "GPIO",
+            "value": data
+        }
+        self.sendMessage(json.dumps(gpioMessage).encode('utf8'))
+
     async def sensorRFID(self, callback):
+
+        #rfid1 = '182.160.142.141'
+        MIFAREReader = MFRC522.MFRC522()
+
         while True:
+            (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+            (status,uid) = MIFAREReader.MFRC522_Anticoll()
+            if status == MIFAREReader.MI_OK:
+                self.rfidFlag = True
+                await callback(str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3]))
             #check here for rfid registration
             #callback(rfid)
-            pass
+            #pass
 
 
     async def sensorGPIO(self, callback):
         while True:
+            if self.rfidFlag == True and self.serverFlag == True:
+                if GPIO.input(18) == GPIO.HIGH :
+                    #print("sensorGPIO")
+                    await callback("1")
+
             #check here for rfid registration
-            print("sensorGPIO")
-            await callback("test")
+           # print("sensorGPIO")
+            #await callback("test")
 
 
     async def display_spinner(self, char=cycle('/|\-')):
